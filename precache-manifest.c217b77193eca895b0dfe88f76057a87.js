181a26bc3e5bbd7c4c8c65c3736a42553f837fa2
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-04-06 00:50:07 +0530

    Updates

---
+++ precache-manifest.c217b77193eca895b0dfe88f76057a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa2f0a6e066a69d4c133443332a3a83",
+    "url": "/React-TodoList-app/index.html"
+  },
+  {
+    "revision": "d54c0ca16286c76729bb",
+    "url": "/React-TodoList-app/static/css/main.51a08a46.chunk.css"
+  },
+  {
+    "revision": "f9bef04c3e4f06e1e195",
+    "url": "/React-TodoList-app/static/js/2.3d4bca9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-TodoList-app/static/js/2.3d4bca9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54c0ca16286c76729bb",
+    "url": "/React-TodoList-app/static/js/main.51345f68.chunk.js"
+  },
+  {
+    "revision": "d67f03f1dd6fd1ba0a51",
+    "url": "/React-TodoList-app/static/js/runtime-main.dbb0be6f.js"
+  }
+]);
